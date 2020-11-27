53e8a447caca9bfac5e5dde144f8ce7bab951a92
HenryTan0705 <htan0705@gmail.com>
2020-11-27 12:37:46 -0500

    Display Tie if game is over with no winner

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -27,7 +27,7 @@ export class BoardComponent implements OnInit {
   }
 
   makeMove(squareIdx: number) {
-    if (this.winner) return;
+    if (this.gameOver()) return;
 
     // if square is empty fill in square
     if (!this.squares[squareIdx]) {
@@ -62,5 +62,10 @@ export class BoardComponent implements OnInit {
       }
     }
     return null;
+  }
+
+  gameOver() {
+    if (this.winner) return true;
+    if (this.squares.every(val => val !== null)) return true;
   }
 }
